d6bedb6ba3804325f7a1cd8ab3a7c2e011c384ad
iyukukuku <nurruddinafif16@gmail.com>
2022-03-08 14:54:18 +0700

    Create PopUp.js

---
+++ Assets/js/PopUp.js
@@ -0,0 +1,6 @@
+function daftar() {
+    window.alert("Terima Kasih, Akun anda berhasil terdaftar, Silahkan Masuk Menggunakan Email dan Passowrd");
+}
+function masuk() {
+    window.alert("Selamat Datang di Pesona Jember");
+}
